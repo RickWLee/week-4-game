6ae485e4d469df949eede1faba29bd2519292e2d
Weihao Rick Lee <kenchi133@hotmail.com>
2016-09-10 22:33:14 -0500

    Redo charactor selection on the RPG and make the user friendly.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,11 @@ $( document ).ready(function() {
     	{name: 'vader', hp: 180, attk: 70, cattk:75, img: 'assets/images/2.jpg', mydiv: $("<div>").attr('id', 'vader')},	
     	{name: 'obi', hp: 140, attk:50, cattk:50, img: 'assets/images/3.jpg', mydiv: $("<div>").attr('id', 'obi')},
     	{name: 'kyloren', hp: 110, attk:30, cattk: 35, img: 'assets/images/4.jpg', mydiv: $("<div>").attr('id', 'kyloren')},
-  /*      {name: 'rey', hp: 100, attk:50, cattk:50, img: 'assets/images/5.jpg'},
-        {name: 'luke', hp: 160, attk:60, cattk: 45, img: 'assets/images/6.jpg'},*/
+        {name: 'rey', hp: 100, attk:50, cattk:50, img: 'assets/images/5.jpg'},
+        {name: 'luke', hp: 160, attk:60, cattk: 45, img: 'assets/images/6.jpg'},
     ];
 
-// var newDiv = $('<div>').html("Some text");
-// var newH1 = $('<h1>').html("hello");
-
-// newDiv.append(newH1);
-
-// <div>
-//   Some Text
-//   <h1>hello</h1>
-// </div>
-
-
-
-// $("#container").append(newDiv);
-
-
-
-/*Create Button*/
-
+//show available players to choose from
     for (var i=0; i<players.length; i++) {
         
         var b = $('<button>');
@@ -34,47 +17,48 @@ $( document ).ready(function() {
             b.attr('data-fighter',players[i]);
             b.append(players[i].name +"<br><img src='"+players[i].img+"'<br><br>"+players[i].hp);
             $('#showPlayers').append(b);
-        };
+    };
+
+
         var playerChoosen=false;
         var defenderChosen=false;
-    
 
-     $('.button-border').on('click', function() {
+    //hide all sections so user can only choose from the list of players
+        $('#selectedPlayerSection').hide();
+        $('#defenderSection').hide();
+        $('#showOpponentsSection').hide();
+        $('#duelStatusSection').hide();
+
+    //On click, user will choose the players and it will show the selected player and list other players as opponents for attack.
+
+    $('.button-border').on('click', function() {
         if (!playerChoosen){
+                console.log(playerChoosen);
 
                 $("#selectedPlayer").append($(this));
                 $("#showOpponents").append($("#showPlayers").children());
-               
-        playerChoosen=true;
+                $('#showPlayersSection').hide(); //remove the empty div for show all players
+                
+                playerChoosen=true;
+                $('#selectedPlayerSection').show(); //Allow user to see the selected player
+                $('#showOpponentsSection').show(); //Allow user to see the avaliable opponents for selection for fight
+                
 
-    } else if (!defenderChosen){
+        } else if (!defenderChosen){
+                $('#defenderSection').show(); //after user select the opponent, defender section will be display
+                $('#duelStatusSection').show(); //Duel status will be display as defender has been selected.
 
-                $ ("#selectedOpponent").append($(this)); 
-                }   
-            });
+                console.log(playerChoosen);
+                console.log(defenderChosen);
+                
+                $("#selectedOpponent").append($(this)); 
+                $("#selectedPlayer: parent").remove("button-border");  
+
+                }  
+
+    });
 
          
-      
-  
-
-
-/*
-    $('.button-border').on('click', function() {
-        $("#selectedPlayer").append($(this));
-        $("#showOpponents").append($('#showPlayers').children());
-        });
-
-     $('.buttonOppo').on('click', function() {
-        $("#selectedOppo")append($(this));
-        });
-
-  $(this).hide();
-  
-    $('#.button-border').on('click', function() {
-        $("#selectedOpponent").append($(this));
-        });
-
-*/ 
 
 
 });
